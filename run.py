452409f8ac281755dcaf382e88c7e9d692384832
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-06-11 23:19:14 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "2190cj7jf0sda90902j8"
 
 posts = [
     {
@@ -12,12 +14,25 @@ posts = [
 
 @app.route("/home")
 @app.route("/")
-def index():
+def home():
     return render_template("home.html", post = posts , title = "Home")
 
 @app.route("/about")
 def about():
     return render_template("about.html", title = "About")
+
+@app.route("/register", methods =["POST","GET"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created {form.username.data}!", "succes")
+    return render_template("register.html", title="Register",  form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login",  form = form)
+    
     
 
 if __name__ == "__main__":
