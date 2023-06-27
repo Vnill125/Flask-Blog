12cf126b7272930051a7adaba2bb6ebf10de2d7c
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-06-27 23:32:56 +0200

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "2190cj7jf0sda90902j8"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 db = SQLAlchemy(app)
 bc = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 from flaskblog import routes
