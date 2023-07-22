668f3d29975f352f46d230595c9840589c99fdc1
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-07-22 13:47:19 +0200

    Update forms.py

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -45,4 +45,3 @@ class UpdateAccountForm(FlaskForm):
             email = User.query.filter_by(email=email.data).first()
             if email:
                 raise ValidationError('That email is taken. Please choose a different one.')
-
